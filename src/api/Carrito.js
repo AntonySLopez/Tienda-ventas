bc10a630496ef234304d2f36faa6249eeb6826e6
antony.sl <antony.g.s.lopez@gmail.com>
2025-05-20 02:40:27 -0500

    Agregado 2 hojas e items corespondientes

---
+++ src/api/Carrito.js
@@ -0,0 +1,7 @@
+export function CarItems (){
+    return [
+        { name:"cobrizo", cantidad: 10, precio: 40 },
+        { name:"capterm", cantidad: 10, Precio: 30 },
+        { name:"topazot", cantidad: 10, Precio: 10 },
+    ]
+}
