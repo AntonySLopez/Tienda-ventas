550834eafba045cff482108cc2b92fe51c696ccd
antony.sl <antony.g.s.lopez@gmail.com>
2025-05-21 00:53:07 -0500

    Agregado de hojas completo, falta item de producto script y fetch

--- src/api/Carrito.js
+++ src/api/Carrito.js
@@ -1,7 +1,11 @@
 export function CarItems (){
     return [
-        { name:"cobrizo", cantidad: 10, precio: 40 },
-        { name:"capterm", cantidad: 10, Precio: 30 },
-        { name:"topazot", cantidad: 10, Precio: 10 },
+        { foto:"../../public/gals_logo_200px.webp", name:"cobrizo", cantidad: 10, precio: 40 },
+        { foto:"../../public/gals_logo_200px.webp", name:"capterm", cantidad: 10, Precio: 30 },
+        { foto:"../../public/gals_logo_200px.webp", name:"topazot", cantidad: 10, Precio: 10 },
+        { foto:"../../public/gals_logo_200px.webp", name:"topazot", cantidad: 10, Precio: 10 },
+        { foto:"../../public/gals_logo_200px.webp", name:"topazot", cantidad: 10, Precio: 10 },
+        { foto:"../../public/gals_logo_200px.webp", name:"topazot", cantidad: 10, Precio: 10 },
+        { foto:"../../public/gals_logo_200px.webp", name:"topazot", cantidad: 10, Precio: 10 }
     ]
 }
